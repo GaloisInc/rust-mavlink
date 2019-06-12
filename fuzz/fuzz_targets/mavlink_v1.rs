828b4a6a1ed3db55170a7968f292d7b85a1acc1d
Michal Podhradsky <mpodhradsky@galois.com>
2019-06-11 17:43:00 -0700

    Added fuzz targets

---
+++ fuzz/fuzz_targets/mavlink_v1.rs
@@ -0,0 +1,13 @@
+#![no_main]
+#[macro_use] extern crate libfuzzer_sys;
+extern crate mavlink;
+
+fuzz_target!(|data: &[u8]| {
+    // fuzzed code goes here
+    // clone the data so it can be mutable
+    let mut data = data.clone();
+    // now attempt to parse the message
+    // We expect either a valid message or an error
+    // If we discover a panic!() or a segfault, we got a problem
+    let _ = mavlink::read_v1_msg(&mut data);
+});
